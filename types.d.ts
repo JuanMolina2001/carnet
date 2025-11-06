c04b962f191f24dd062d4e032a1a9c019cc19ed1
JuanMolina2001 <juanmolinajm2001@gmail.com>
2025-11-06 17:38:47 -0300

    Refactor Cuartel interface and update type definitions in types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,28 +1,39 @@
-interface Cuartel  {
-  region : string ,
-  unidad_nombre : string ,
-  tipo_unidad : string ,
-  nombre_de : string ,
-  numero : string ,
-  longitude : number ,
-  latitude : number ,
-  an : string,
-geohash : string
+declare module "*.json" {
+  const value: any;
+  export default value;
 }
-type RootStackParamList =  {
+
+interface Cuartel { id: string; region: string; nombre: string; tipo_unidad: string; latitude: number; longitude: number; ano: string; }
+interface UserData {
+  email?: string,
+  password?: string,
+  confirmPassword?: string,
+  rut?: string,
+  name?: string,
+  lastName?: string,
+}
+// Context types
+interface RegisterContextType {
+  userData: UserData,
+  setUserData: (userData: UserData) => void,
+  handleLogin: () => void,
+}
+
+// Navigation types
+type RootStackParamList = {
   Home: undefined;
   Auth: undefined;
   Document: undefined;
 };
-type AuthStackParamList =  {
+type AuthStackParamList = {
   Login: undefined;
   Register: undefined;
 };
-type DocumentStackParamList =  {
+type DocumentStackParamList = {
   TypeDoc: undefined;
   Map: undefined;
 };
-type RegisterStackParamList =  {
+type RegisterStackParamList = {
   Step1: undefined;
   Step2: undefined;
   Biometrics: undefined;
