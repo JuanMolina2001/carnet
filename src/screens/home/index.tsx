6926e92226f1d41ee8b44594c091f7f7f03740a2
JuanMolina2001 <juanmolinajm2001@gmail.com>
2025-11-06 17:38:25 -0300

    Add sign-out functionality to Home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import { Button } from 'react-native-paper';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-
-
+import { auth } from 'firebaseConfig';
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 export const Home = ({ route, navigation }: Props) => {
   return (
     <View>
         <Text>Home Screen</Text>
         <Button mode="contained" onPress={() => navigation.navigate('Document')}>Publicar Documento</Button>
+        <Button mode="contained" onPress={() => {
+            auth.signOut();
+        }}>Cerrar sesion</Button>
     </View>
   )
 }
